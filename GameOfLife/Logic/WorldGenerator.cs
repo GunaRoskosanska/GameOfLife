cae0806f41c04679b7ca9bfc8219eb0d71cadb3b
Guna Roskosanska <guna.roskosanska@gmail.com>
2020-10-08 22:03:33 +0300

    Fix WorldGenerator

--- GameOfLife/Logic/WorldGenerator.cs
+++ GameOfLife/Logic/WorldGenerator.cs
@@ -48,56 +48,33 @@ namespace GameOfLife.Logic
         /// <summary>
         /// Generates grid for the next generation based on current generation.
         /// </summary>
-        /// <param name="lifeGenerationGrid">Used to specify life generation grid.</param>
-        public WorldGenerationResult NextGeneration(CellStatus[,] lifeGenerationGrid)
+        /// <param name="currentGeneration">Used to specify life generation grid.</param>
+        public WorldGenerationResult NextGeneration(CellStatus[,] currentGeneration)
         {
             var isWorldAlive = false;
             int aliveCells = 0;
             var worldSize = new WorldSize
             {
-                Rows = lifeGenerationGrid.GetUpperBound(0) + 1,
-                Columns = lifeGenerationGrid.GetUpperBound(1) + 1,
+                Rows = currentGeneration.GetUpperBound(0) + 1,
+                Columns = currentGeneration.GetUpperBound(1) + 1,
             };
             var nextGeneration = new CellStatus[worldSize.Rows, worldSize.Columns];
 
             // Loop through every cell
-            for (var row = 1; row < worldSize.Rows - 1; row++)
+            for (var row = 0; row < worldSize.Rows; row++)
             {
-                for (var column = 1; column < worldSize.Columns - 1; column++)
+                for (var column = 0; column < worldSize.Columns; column++)
                 {
                     // Find alive neighbors
-                    var aliveNeighbors = 0;
-                    for (var i = -1; i <= 1; i++)
-                    {
-                        for (var j = -1; j <= 1; j++)
-                        {
-                            aliveNeighbors += lifeGenerationGrid[row + i, column + j] == CellStatus.Alive ? 1 : 0;
-                        }
-                    }
-                    var currentCell = lifeGenerationGrid[row, column];
+                    var aliveNeighbors = CalculateLiveNeighbours(row, column, currentGeneration);
+                  
+                    var currentCell = currentGeneration[row, column];
+                    
+                    var judgment = Judge(currentCell, aliveNeighbors);
 
-                    // The cell needs to be removed from its neighbors as it was counted before
-                    aliveNeighbors -= currentCell == CellStatus.Alive ? 1 : 0;
+                    nextGeneration[row, column] = judgment;
 
-                    // Implementing the rules of life
-                    if (currentCell == CellStatus.Alive && aliveNeighbors < 2) // Cell is lonely and dies
-                    {
-                        nextGeneration[row, column] = CellStatus.Dead;
-                    }
-                    else if (currentCell == CellStatus.Alive && aliveNeighbors > 3) // Cell dies due to over population
-                    {
-                        nextGeneration[row, column] = CellStatus.Dead;
-                    }
-                    else if (currentCell == CellStatus.Dead && aliveNeighbors == 3) // A new cell is born
-                    {
-                        nextGeneration[row, column] = CellStatus.Alive;
-                    }
-                    else // Stays the same
-                    {
-                        nextGeneration[row, column] = currentCell;
-                    }
-
-                    if (currentCell == CellStatus.Alive)
+                    if (judgment == CellStatus.Alive)
                     {
                         aliveCells++;
                     }
@@ -115,6 +92,65 @@ namespace GameOfLife.Logic
                 IsGenerationAlive = isWorldAlive,
                 Generation = nextGeneration
             };
+        }
+
+        /// <summary>
+        /// Makes decision whether a cell remains alive or dead
+        /// </summary>
+        private CellStatus Judge(CellStatus cell, int aliveNeighbors)
+        {
+            // Implementing the rules of life
+            if (cell == CellStatus.Alive && aliveNeighbors < 2) // Cell is lonely and dies
+            {
+                return CellStatus.Dead;
+            }
+            else if (cell == CellStatus.Alive && aliveNeighbors > 3) // Cell dies due to over population
+            {
+                return CellStatus.Dead;
+            }
+            else if (cell == CellStatus.Dead && aliveNeighbors == 3) // A new cell is born
+            {
+                return CellStatus.Alive;
+            }
+            else // Stays the same
+            {
+                return cell;
+            }
+        }
+
+        /// <summary>
+        /// Given any cell - calculate live neighbours
+        /// </summary>
+        /// <param name="x">X coord of Cell</param>
+        /// <param name="y">Y coord of Cell</param>
+        /// <returns>Number of live neighbours</returns>
+        private int CalculateLiveNeighbours(int x, int y, CellStatus[,] currentGeneration)
+        {
+            var size = new WorldSize
+            {
+                Rows = currentGeneration.GetUpperBound(0) + 1,
+                Columns = currentGeneration.GetUpperBound(1) + 1,
+            };
+
+            // Calculate live neighours
+            int liveNeighbours = 0;
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (x + i < 0 || x + i >= size.Rows)   // Out of bounds
+                        continue;
+                    if (y + j < 0 || y + j >= size.Columns)   // Out of bounds
+                        continue;
+                    if (x + i == x && y + j == y)       // Same Cell
+                        continue;
+
+                    // Add cells value to current live neighbour count
+                    liveNeighbours += (int)currentGeneration[x + i, y + j];
+                }
+            }
+
+            return liveNeighbours;
         }
     }
 }
